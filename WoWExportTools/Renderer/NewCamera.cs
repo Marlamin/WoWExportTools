1dff3a09141e43629b83d836ea61ba5dc42c0981
Kruithne <kruithne@gmail.com>
2019-04-22 21:21:48 +0100

    Remove unused import statements from NewCamera

--- WoWExportTools/Renderer/NewCamera.cs
+++ WoWExportTools/Renderer/NewCamera.cs
@@ -2,10 +2,6 @@
 using OpenTK.Graphics.OpenGL;
 using OpenTK.Input;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace OBJExporterUI
 {
