60c6e06b6bec42eeaf1062f5f7c6f1a5193b52ae
Martin Benjamins <marlamin@marlamin.com>
2019-06-22 17:15:29 +0200

    Rename assemblies to WoWExportTools

--- WoWExportTools/Properties/Resources.Designer.cs
+++ WoWExportTools/Properties/Resources.Designer.cs
@@ -39,7 +39,7 @@ namespace WoWExportTools.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("OBJExporterUI.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("WoWExportTools.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
