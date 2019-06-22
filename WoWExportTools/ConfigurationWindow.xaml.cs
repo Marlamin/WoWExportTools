c7f060fd42034549e297013a5fd648dbd81cb80d
Martin Benjamins <marlamin@marlamin.com>
2019-06-22 17:07:22 +0200

    Temporarily disable Classic in program selection

--- WoWExportTools/ConfigurationWindow.xaml.cs
+++ WoWExportTools/ConfigurationWindow.xaml.cs
@@ -142,8 +142,8 @@ namespace OBJExporterUI
             programSelect.Items.Add(new KeyValuePair<string, string>("Live (Retail)", "wow"));
             programSelect.Items.Add(new KeyValuePair<string, string>("Public Test Realm (PTR)", "wowt"));
             programSelect.Items.Add(new KeyValuePair<string, string>("Beta", "wow_beta"));
-            programSelect.Items.Add(new KeyValuePair<string, string>("Classic (Retail)", "wow_classic"));
-            programSelect.Items.Add(new KeyValuePair<string, string>("Classic Beta", "wow_classic_beta"));
+            //programSelect.Items.Add(new KeyValuePair<string, string>("Classic (Retail)", "wow_classic"));
+            //programSelect.Items.Add(new KeyValuePair<string, string>("Classic Beta", "wow_classic_beta"));
             programSelect.Items.Add(new KeyValuePair<string, string>("Submission (unknown use)", "wowz"));
             programSelect.DisplayMemberPath = "Key";
             if (_editMode)
